55ff6c8c1f0cff6aabccff3c7b2cf21aa7b07370
CodeArtist7071 <artistproysn@outlook.com>
2023-12-24 12:50:09 +0530

    App.js files

--- zomeal/App.js
+++ zomeal/App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { ScreenStackHeaderCenterView } from 'react-native-screens';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Routes from './routes/Routes';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Routes/>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
