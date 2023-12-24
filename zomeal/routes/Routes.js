849e586d6934859d9825852389e1e5c57d5a8321
CodeArtist7071 <artistproysn@outlook.com>
2023-12-24 12:57:10 +0530

    please check the router

--- zomeal/routes/Routes.js
+++ zomeal/routes/Routes.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { ScreenStackHeaderCenterView } from 'react-native-screens';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../src/pages/LoginScreen';
 import SignUpScreen from '../src/pages/SignUpScreen';
@@ -17,37 +16,12 @@ export default function Routes() {
   return (
  <NavigationContainer>
   <Stack.Navigator>
-    <Stack.Screen
-    name="SubMainScreen"
-    component={SubMainScreen}
-    options={{headerShown:false}}
-    
-    />
-    <Stack.Screen 
-    name='Signup'
-    component={SignUpScreen}
-    
-    />
+  
     <Stack.Screen 
     name='Login'
     component={LoginScreen}
     />
-     <Stack.Screen 
-    name='forgetpassword'
-    component={ForgetPassword}
-    />
-    <Stack.Screen
-    name='pincode'
-    component={PincodeScreen}/>
-    <Stack.Screen
-    name='dashboard'
-    component={Dashboard}/>
-     <Stack.Screen
-    name='termsCondition'
-    component={TermsCondition}/>
-    <Stack.Screen
-    name='menu'
-    component={Menu}/>
+
   </Stack.Navigator>
  </NavigationContainer>
     
